ca3b5ebc52dbf53be71854e3c9ce74c6eb4c71e4
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-06-04 21:36:22 +0200

    abc

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json } from 'react-router-dom';
+import { useRouteLoaderData, json, redirect } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 export default function EventDetailPage() {
   const data = useRouteLoaderData('event-detail');
@@ -7,7 +7,7 @@ export default function EventDetailPage() {
 
 export async function loader({ request, params }) {
   const id = params.eventId;
-  
+
   const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
@@ -20,4 +20,20 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+  });
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not delete event.' },
+      {
+        status: 500,
+      }
+    );
+  } 
+  return redirect('/events')
 }
